fada4d149df8c729240411b7b0aaadfd468b9515
Damian Sznajder <dsznajder@users.noreply.github.com>
2022-06-29 13:19:37 -0700

    chore: migrate @binance-chain/javascript-sdk to @thorswap-lib/binance-chain-sdk (#121)

--- packages/xchain-binance/jest.config.js
+++ packages/xchain-binance/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
   testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/lib'],
   setupFilesAfterEnv: ['./jest.setup.js'],
 }
